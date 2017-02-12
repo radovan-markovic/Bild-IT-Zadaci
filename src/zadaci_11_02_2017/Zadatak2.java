679129dd42be720a1e5b85179a5091a6554f6b11
Radovan <Radovan@DESKTOP-AMGF16D>
2017-02-12 16:46:44 +0100

    zadaci 11.02.2017.

--- src/zadaci_11_02_2017/Zadatak2.java
+++ src/zadaci_11_02_2017/Zadatak2.java
@@ -15,21 +15,19 @@ public class Zadatak2 {
 
 		//provjeravamo unos
 		String provjeriDuzinu = broj+"";
-
-		while (provjeriDuzinu.length() != 9){
-			System.out.println("Pogresan unos, unesite 9 brojeva:");
-			broj = input.nextLine();
-			provjeriDuzinu = broj+"";
-
-			for (int j = 0; j<broj.length(); j++){
-				if (Character.isLetter(broj.charAt(j))){
-					System.out.println("Pogresan unos, unesite  9 brojeva:");
+		
+		while (test){
+			for (int a = 0; a<broj.length(); a++){
+				while (provjeriDuzinu.length() != 9 || !Character.isDigit(broj.charAt(a))){	
+					System.out.println("Pogresan unos, unesite 9 brojeva:");
 					broj = input.nextLine();
 					provjeriDuzinu = broj+"";
 				}
+			
 			}
+			test = false;
 		}	
-
+		
 		int suma = 0;
 		int index = 0;
 		
